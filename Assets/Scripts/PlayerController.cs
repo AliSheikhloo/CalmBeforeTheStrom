f1a1b54c361c4f544b6c9257984f8724a453dbd1
Erfankhodami <khodami.erfan@gmail.com>
2024-09-09 20:30:05 +0330

    added basic movment and added floor to the scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float MovingSpeedF;
+
+    private Rigidbody2D PlayerRB;
+    private bool IsShiftPressedB=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !IsShiftPressedB)
+        {
+            MovingSpeedF *= 2;
+            IsShiftPressedB = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            IsShiftPressedB = false;
+            MovingSpeedF /= 2;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerRB.AddForce(-transform.right * MovingSpeedF);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            PlayerRB.AddForce(-transform.up * MovingSpeedF);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerRB.AddForce(transform.right * MovingSpeedF);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            PlayerRB.AddForce(transform.up * MovingSpeedF);
+        }
+    }
+}
